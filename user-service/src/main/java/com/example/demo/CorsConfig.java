9d4352f2a9d845ace0336c4fb173cfa8fd7a641d
vishalkashid08 <vishalkashid08@gmail.com>
2026-06-22 13:54:35 +0530

    d

--- user-service/src/main/java/com/example/demo/CorsConfig.java
+++ user-service/src/main/java/com/example/demo/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
