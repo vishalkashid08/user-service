786ea87cb90a3982969cb9e227c339e19f180b49
vishalkashid08 <vishalkashid08@gmail.com>
2026-06-22 12:07:28 +0530

    * added

--- user-service/src/main/java/com/example/demo/security/SecurityConfig.java
+++ user-service/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://3.110.167.15:5173"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
